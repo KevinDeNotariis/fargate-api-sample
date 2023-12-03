9a31365ff61c44202cb4a79e1d4dc6ada369d05b
Kevin De Notariis <kevin.denotariis@gmail.com>
2023-12-03 16:25:42 +0000

    fix: Fixing import path for auth middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { CustomError } from '../utils/errorHandlers/CustomError';
 
 import apiRoutes from './api';
-import { validateApiKey } from 'controllers/authentication';
+import { validateApiKey } from '../controllers/authentication';
 
 const router = Router();
 
